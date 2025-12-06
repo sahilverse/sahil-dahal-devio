316459c6e4e88ae1813a9a2c8c4e1b3a46098238
Sahil Dahal <denenme49@gmail.com>
2025-12-06 13:48:59 +0545

    Add UserRepository and AuthController symbols to TYPES

--- apps/backend/src/types/index.ts
+++ apps/backend/src/types/index.ts
@@ -2,4 +2,6 @@ export const TYPES = {
     AuthService: Symbol.for("AuthService"),
     UserService: Symbol.for("UserService"),
     RedisManager: Symbol.for("RedisManager"),
+    UserRepository: Symbol.for("UserRepository"),
+    AuthController: Symbol.for("AuthController"),
 };
