ece9952ec6a816fb6f7b0f85d4c779103121e236
Sahil Dahal <denenme49@gmail.com>
2025-12-06 13:49:15 +0545

    Add removePasswordFromUser utility function to index file

--- apps/backend/src/utils/index.ts
+++ apps/backend/src/utils/index.ts
@@ -4,3 +4,9 @@ export * from './logger';
 export * from './response';
 export * from './bcrypt';
 export * from './jwt';
+
+
+export const removePasswordFromUser = <T extends { password?: string }>(obj: T): Omit<T, 'password'> => {
+    const { password, ...rest } = obj;
+    return rest;
+}
