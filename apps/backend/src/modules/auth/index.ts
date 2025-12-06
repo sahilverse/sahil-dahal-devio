792798c4a763566c506fbbcd77b71ce520ec9861
Sahil Dahal <denenme49@gmail.com>
2025-12-06 13:48:49 +0545

    Export additional auth module components in index file

--- apps/backend/src/modules/auth/index.ts
+++ apps/backend/src/modules/auth/index.ts
@@ -1,1 +1,5 @@
-export * from './auth.route'
+export * from './auth.route';
+export * from './auth.controller';
+export * from './auth.service';
+export * from './auth.types';
+export * from './auth.repository';
