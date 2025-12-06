e08236989eafb69ae6b37963d5b74ecd205008d8
Sahil Dahal <denenme49@gmail.com>
2025-12-06 13:48:29 +0545

    Add AuthService, AuthController, and UserRepository bindings to Inversify container

--- apps/backend/src/config/inversify.ts
+++ apps/backend/src/config/inversify.ts
@@ -1,10 +1,16 @@
 import { Container } from "inversify";
 import { TYPES } from "../types";
 import { RedisManager } from "./redis";
+import { AuthService, AuthController } from "../modules/auth";
+import { UserRepository } from "../modules/user";
 
 
 const container = new Container();
 
 container.bind<RedisManager>(TYPES.RedisManager).to(RedisManager).inSingletonScope();
+container.bind<AuthService>(TYPES.AuthService).to(AuthService).inSingletonScope();
+container.bind<UserRepository>(TYPES.UserRepository).to(UserRepository).inSingletonScope();
+container.bind<AuthController>(TYPES.AuthController).to(AuthController).inSingletonScope();
+
 
 export { container };
