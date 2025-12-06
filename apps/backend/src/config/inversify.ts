7f48a4c70bc20537539d03e25b17cc559d41aed6
Sahil Dahal <denenme49@gmail.com>
2025-12-06 14:09:37 +0545

    Add AuthRepository binding to Inversify container and adjust PrismaClient binding

--- apps/backend/src/config/inversify.ts
+++ apps/backend/src/config/inversify.ts
@@ -1,16 +1,24 @@
 import { Container } from "inversify";
 import { TYPES } from "../types";
 import { RedisManager } from "./redis";
-import { AuthService, AuthController } from "../modules/auth";
+import { AuthService, AuthController, AuthRepository } from "../modules/auth";
 import { UserRepository } from "../modules/user";
+import { prisma } from "./prisma";
 
 
 const container = new Container();
 
 container.bind<RedisManager>(TYPES.RedisManager).to(RedisManager).inSingletonScope();
+container.bind<typeof prisma>(TYPES.PrismaClient).toConstantValue(prisma);
+
+container.bind<AuthRepository>(TYPES.AuthRepository).to(AuthRepository).inSingletonScope();
 container.bind<AuthService>(TYPES.AuthService).to(AuthService).inSingletonScope();
-container.bind<UserRepository>(TYPES.UserRepository).to(UserRepository).inSingletonScope();
 container.bind<AuthController>(TYPES.AuthController).to(AuthController).inSingletonScope();
+
+container.bind<UserRepository>(TYPES.UserRepository).to(UserRepository).inSingletonScope();
+
+
+
 
 
 export { container };
