4ef13d0f80e499be3e97bd2e9c6467e17123e340
Sahil Dahal <denenme49@gmail.com>
2025-12-06 12:08:00 +0545

    Implement server startup and graceful shutdown with Redis and database connections

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,9 +2,52 @@ import 'dotenv/config';
 import "reflect-metadata";
 import app from './app';
 import { PORT } from './config/constants';
+import { createServer } from 'http';
+import { logger } from './utils';
+import { prisma } from './config'
+import { RedisManager } from './config';
+
+const server = createServer(app);
 
 
-// TODO: Connect to Database and create a bootstrap function + graceful shutdown
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+    try {
+        await RedisManager.init();
+        logger.info("Connected to Redis");
+
+        await prisma.$connect();
+        logger.info("Connected to the database");
+
+
+        server.listen(PORT, () => {
+            console.log(`🚀 Server is running at http://localhost:${PORT}`);
+        });
+
+        process.on("SIGINT", shutdown);
+        process.on("SIGTERM", shutdown);
+
+    } catch (error: any) {
+        logger.error("Failed to start server:", error);
+        process.exit(1);
+    }
+}
+
+
+
+async function shutdown() {
+    logger.info("Shutting down...");
+    try {
+        server.close(async () => {
+            await prisma.$disconnect();
+            await RedisManager.disconnect();
+
+            logger.info("Server closed");
+            process.exit(0);
+        });
+    } catch (err: any) {
+        logger.error("❌ Error during shutdown:", err);
+        process.exit(1);
+    }
+}
+
+startServer();
