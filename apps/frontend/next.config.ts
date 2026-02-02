1bc9f3f29edb8809d3008d05f9d35c338d1d0976
Sahil Dahal <denenme49@gmail.com>
2026-02-02 23:39:45 +0545

    Add: Redirection for u/ to user/

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/u/:username',
+        destination: '/user/:username',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
