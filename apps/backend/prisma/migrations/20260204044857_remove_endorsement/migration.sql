e9987631c879ee2a1684ed64dd394bcc37018550
Sahil Dahal <denenme49@gmail.com>
2026-02-04 10:34:52 +0545

    Update: Remove endorsement from UserSkill table

---
+++ apps/backend/prisma/migrations/20260204044857_remove_endorsement/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `endorsements` on the `UserSkill` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UserSkill" DROP COLUMN "endorsements";
