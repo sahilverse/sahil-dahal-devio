8a0be3305af900ecfce2a61a20b04c4c7e92d690
Sahil Dahal <denenme49@gmail.com>
2026-02-08 02:40:22 +0545

    feat: remove community.route export from index file

--- apps/backend/src/modules/community/index.ts
+++ apps/backend/src/modules/community/index.ts
@@ -2,4 +2,4 @@ export * from "./community.repository";
 export * from "./community.service";
 export * from "./community.controller";
 export * from "./community.dto";
-export * from "./community.route";
+
