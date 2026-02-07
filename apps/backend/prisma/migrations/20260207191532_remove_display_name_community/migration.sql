4f06942d3e7f21f3eb400b338da908e0199e2ea9
Sahil Dahal <denenme49@gmail.com>
2026-02-08 02:31:09 +0545

    feat: remove display_name column from Community model and update migration script

---
+++ apps/backend/prisma/migrations/20260207191532_remove_display_name_community/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `display_name` on the `Community` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Community" DROP COLUMN "display_name";
